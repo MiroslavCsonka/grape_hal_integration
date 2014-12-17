f82d5228e90b1be35f16a6b3abe0f7f10d5c8e63
Miroslav Csonka <miroslav.csonka@gmail.com>
2014-12-17 05:11:51 +0100

    Implemented autoloading of resources, autogenerating hal

--- lib/grape_hal_integration.rb
+++ lib/grape_hal_integration.rb
@@ -4,15 +4,36 @@ class GrapeHalIntegration < Grape::API
 
   @@endpoints = {}
 
-  def self.build_action(name)
-    "#{self.name}-#{name.to_s}"
+  def self.links(class_name, action_name)
+    links = class_name.endpoint(action_name)[:links]
+    _links = Hash[links.map { |link|
+
+                    if link.include? '#'
+                      class_name2, action_name2 = link.split '#'
+                      name = action_name2
+                      endpoint = class_name2.constantize.endpoint(action_name2)
+                    else
+                      name = link
+                      endpoint = class_name.endpoint(link)
+                    end
+
+                    raise "Unregistered link to '#{link}'" if endpoint.nil?
+
+                    return [name, endpoint[:self]]
+                  }]
+    _links[:self] = class_name.endpoint(action_name)[:self]
+    _links
   end
 
-  def self.implement_new(name, url, links = [], &block)
+  def self.build_current_action(name)
+    "#{self.name}##{name.to_s}"
+  end
+
+  def self.implement(name, url, links = [], &block)
     name = name.to_s
     http_verb, url = url.split ' '
-    get_params_name = url.scan(/\{([^}]*)\}/).flatten
-    @@endpoints[build_action(name)] = {
+
+    @@endpoints[build_current_action(name)] = {
         name: name,
         links: links,
         block: block,
@@ -21,28 +42,39 @@ class GrapeHalIntegration < Grape::API
             method: http_verb.upcase
         }
     }
+    @@endpoints[build_current_action(name)][:self][:templated] = true if url.include? '{'
 
-    if url.include? '{'
-      @@endpoints[build_action(name)][:self][:templated] = true
-    end
-
-    super_context = lambda do
+    class_name = self.name.constantize
+    get_params_name = url.scan(/\{([^}]*)\}/).flatten
+    wrapper_block = lambda do
       get_params_values = get_params_name.map do |get_param_name|
-        params[get_param_name.intern]
+        result = get_param_name.match(/(.+)_id/)
+        value = params[get_param_name.intern]
+        unless result.blank?
+          begin
+            klass = result[1].singularize.classify.constantize
+            value = klass.find(value) if klass.class == Class
+          rescue NameError
+          end
+        end
+        value
       end
-      self.instance_exec *get_params_values, &block
+
+      hal = {_links: HalIntegrator.links(class_name, name)}
+      response = self.instance_exec *get_params_values, &block
+      hal.merge(response)
     end
 
     resource url.gsub('{', ':').gsub('}', '') do
       case http_verb.upcase
         when 'POST'
-          post &super_context
+          post &wrapper_block
         when 'PUT'
-          put &super_context
+          put &wrapper_block
         when 'DELETE'
-          delete &super_context
+          delete &wrapper_block
         when 'GET'
-          get &super_context
+          get &wrapper_block
         else
           raise "Undefined http verb '#{http_verb}'"
       end
@@ -50,19 +82,17 @@ class GrapeHalIntegration < Grape::API
   end
 
   def self.endpoint(name)
-    @@endpoints[build_action(name.to_s)]
-  end
-
-  def self.method_missing(method_name, *arguments, &block)
-    raise "Unknown name '#{build_action(method_name)}'" unless @@endpoints.has_key? build_action(method_name)
-    context = arguments.shift
-    context.instance_exec *arguments, &endpoint(method_name)[:block]
+    @@endpoints[build_current_action(name.to_s)]
   end
 
   def self.endpoint_url(name)
-    e = endpoint(name)
-    raise "Could not find endpoint '#{name}'" unless e.is_a? Hash
-    e[:self]
+    endpoint(name)[:self]
+  end
+
+  def self.method_missing(method_name, *arguments, &block)
+    raise "Unknown name '#{build_current_action(method_name)}'" unless @@endpoints.has_key? build_current_action(method_name)
+    context = arguments.shift
+    context.instance_exec *arguments, &endpoint(method_name)[:block]
   end
 
 end
